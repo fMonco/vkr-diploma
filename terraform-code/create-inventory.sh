23ded3ecb40ca3570e0997a15b1c2baad0f7548d
Alexey Avtaykin <alexey.avtaykin@hmhco.com>
2025-02-03 23:04:44 +0300

    Delete

--- terraform-code/create-inventory.sh
+++
@@ -1,9 +0,0 @@
-terraform plan
-terraform apply -auto-approve
-cat <<EOF > ../ansible/inventory.ini
-[gitlab-server]
-gitlab-server ansible_host=$(terraform output -raw external_ip_address_gitlab_server)
-
-[stage]
-stage ansible_host=$(terraform output -raw external_ip_address_stage)
-EOF
